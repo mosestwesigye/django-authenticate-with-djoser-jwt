b15f6362c42e59cba91d82db3d78825c88dc3d2e
mosestwesigye <twesigyemoses1@gmail.com>
2022-01-11 16:11:34 +0300

    master

--- core/accounts/models.py
+++ core/accounts/models.py
@@ -30,11 +30,9 @@ class CustomUserManager(BaseUserManager):
 class CustomUser(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(max_length=255, unique=True)
     username = models.CharField(max_length=255, unique=True)
-    first_name = models.CharField(max_length=255)
-    last_name = models.CharField(max_length=255)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
-    is_admin = models.BooleanField(default=False)
+    #is_admin = models.BooleanField(default=False)
 
     objects = CustomUserManager()
 
